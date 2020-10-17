60a904819e0075e7e41480099eae7c1af85b11ab
Kerry <mrkerrywashington@icloud.com>
2020-10-17 01:06:11 +0100

    Added HTML for support email
    Fixed bitcoin references

--- loafwallet/src/Constants/Constants.swift
+++ loafwallet/src/Constants/Constants.swift
@@ -88,6 +88,63 @@ struct C {
     static var standardPort: Int {
         return E.isTestnet ? 19335 : 9333
     }
+    
+    static let troubleshootingQuestions = """
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+        <html>
+        <head>
+            <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+            <style type="text/css">
+            body {
+                margin:0 0 0 0;
+                padding:0 0 0 0 !important;
+                background-color: #ffffff !important;
+                font-size:12pt;
+                font-family:'Lucida Grande',Verdana,Arial,sans-serif;
+                line-height:14px;
+                color:#303030; }
+            table td {border-collapse: collapse;}
+            td {margin:0;}
+            td img {display:block;}
+            a {color:#865827;text-decoration:underline;}
+            a:hover {text-decoration: underline;color:#865827;}
+            a img {text-decoration:none;}
+            a:visited {color: #865827;}
+            a:active {color: #865827;}
+            p {font-size: 12pt;}
+          </style>
+        </head>
+        <body>
+        <table width="400" border="0" cellpadding="5" cellspacing="5" style="margin: auto;">
+            <tr>
+                <td colspan="2" align="left" style="padding-top:7px; padding-bottom:7px; border-top: 3px solid #777; border-bottom: 1px dotted #777;">
+                    <span style="font-size: 13; line-height: 16px;" face="'Lucida Grande',Verdana,Arial,sans-serif">
+                        <div>Please reply to this email with the following information so that we can prepare to help you solve your Litewallet issue.</div>
+                      <br>
+                         <div>
+                            1. What is the platform of your mobile device (Android or iOS)?</div>
+                          <br>
+                          <br>
+                            <div>
+                            2. What version of software running on your mobile device (e.g.; Android 20, or 24 or iOS 13.7 or iOS 14)?</div>
+                          <br>
+                          <br>
+                            <div>3. What version of Litewallet software is on your mobile device (found on the login view)?</div>
+                          <br>
+                          <br>
+                            <div>4. How we can help?</div>
+                          <br>
+                          <br>
+                    </span>
+                </td>
+          </tr>
+        <br>
+        </table>
+        </body>
+        </html>
+    """
+    
+    
 }
 
 struct AppVersion {
