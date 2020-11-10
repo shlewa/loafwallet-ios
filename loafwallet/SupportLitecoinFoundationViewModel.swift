fbefe842a032d875acfeacd04bfdbb23d164a5b4
Kerry <mrkerrywashington@icloud.com>
2020-11-09 16:45:17 -0800

    Resolved the previous conflicts
    - Added in SupportLitecoinFoundation SwiftUI view and model
    - Working in the basic functionality of the new Support Litecoin Foundation view

---
+++ loafwallet/SupportLitecoinFoundationViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  SupportLitecoinFoundationViewModel.swift
+//  loafwallet
+//
+//  Created by Kerry Washington on 11/9/20.
+//  Copyright © 2020 Litecoin Foundation. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class SupportLitecoinFoundationViewModel: ObservableObject {
+    
+    init() {
+        
+    }
+}
