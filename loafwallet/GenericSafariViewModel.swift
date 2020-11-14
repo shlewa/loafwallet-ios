c591166026dd6dd43adeca2f51f6b85681763a1e
Kerry <mrkerrywashington@icloud.com>
2020-11-14 10:07:50 +0000

    Add new WebKit view and supporting code

---
+++ loafwallet/GenericSafariViewModel.swift
@@ -0,0 +1,19 @@
+//
+//  GenericSafariViewModel.swift
+//  loafwallet
+//
+//  Created by Kerry Washington on 11/13/20.
+//  Copyright © 2020 Litecoin Foundation. All rights reserved.
+//
+
+import Foundation
+
+class GenericSafariViewModel: ObservableObject {
+    
+    @Published
+     var didLoadPage: Bool = false
+    
+    init() {
+        
+    }
+}
